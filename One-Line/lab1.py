3a9d71469ad8810f9e6a7b0a88ea8a4a2f567bc1
Eoin Davey <eoind@vey.ie>
2017-11-21 10:20:54 +0000

    Change lab1

--- One-Line/lab1.py
+++ One-Line/lab1.py
@@ -1,1 +1,1 @@
-print ["VALID","INVALID"][reduce(lambda (x,b),c:(x+[c,[2*c,2*c-9][c>4]][b],not b),map(int,raw_input()[::-1]),(0,False))[0]%10!=0]
+print ["VALID","INVALID"][(lambda s: sum(s+[x-(x/5)*9 for x in s[1::2]]))(map(int,raw_input()[::-1]))%10!=0]
